3e76d0e440e53739a42664f68700c8288d671d61
Ionut Airinei <ionut.n.airinei@gmail.com>
2020-03-06 16:00:16 +0200

    Exercise #1

--- week1/Week1/Program.cs
+++ week1/Week1/Program.cs
@@ -6,7 +6,42 @@ namespace Week1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Exercise1();
+
+            Exercise2();
+             
+        }
+
+        //1. Write a C# Sharp program that takes three letters as input and display them in reverse order.
+        private static void Exercise1()
+        {
+            Console.WriteLine("Write a C# Sharp program that takes three letters as input and display them in reverse order.");
+            
+            var length = 3;
+            var letters = new char[length];
+
+            for (int i = 0; i < length; i++)
+            {
+                letters[i] = Console.ReadKey().KeyChar;
+            }
+
+            Console.WriteLine();
+            Array.Reverse(letters, 0, length);
+            Console.WriteLine(letters);
+        }
+
+
+        //2
+        private static void Exercise2()
+        {
+           /* Console.WriteLine("Write a C# Sharp program that takes two numbers as input and perform an " +
+               "operation (+,-,*,x,/) on them and displays the result of that operation.");
+
+            Console.WriteLine("Input first number:")
+            var firstNumber = Console.ReadLine();
+            var operatie    = Console.ReadLine();
+            var secondNumber = Console.ReadLine();*/
         }
     }
 }
