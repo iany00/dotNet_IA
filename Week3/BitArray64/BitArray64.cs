1fefc81d4025fb2a5cd70656a67d662f81753776
Ionut Airinei <ionut.n.airinei@gmail.com>
2020-03-22 17:06:34 +0200

    BitArray64

--- Week3/BitArray64/BitArray64.cs
+++ Week3/BitArray64/BitArray64.cs
@@ -18,22 +18,107 @@ namespace BitArray64
             set { number = value; }
         }
 
+        public int[] numberInBits { get; set; }
+
         //Constructor
         public BitArray64(ulong number)
         {
-            this.Number = number;
+            this.Number  = number;
+            numberInBits = getBits();
         }
 
         public IEnumerator<int> GetEnumerator()
         {
-            throw new NotImplementedException();
+            int[] bitiArr = this.numberInBits;
+            for (int i = 0; i < bitiArr.Length; i++)
+            {
+                yield return bitiArr[i];
+            }
+        }
+
+        private int[] getBits()
+        {
+            ulong num = this.Number;
+            int[] bitiArray = new int[64];
+
+            // transform
+            string string_64 = Convert.ToString((long)num, 2).PadLeft(64, '0');
+            for (int i = 0; i < string_64.Length; i++)
+            {
+                bitiArray[i] = int.Parse(string_64[i].ToString());
+            }
+
+            return bitiArray;
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return this.GetEnumerator();
         }
 
-        //TODO
+        //Equals
+        public bool Equals(BitArray64 value)
+        {
+            if (value == null)
+            {
+                return false;
+            }
+
+            return this.number == value.number;
+        }
+
+        //==
+        public static bool operator ==(BitArray64 num1, BitArray64 num2)
+        {
+            return BitArray64.Equals(num1, num2);
+        }
+
+
+        //!=
+        public static bool operator != (BitArray64 num1, BitArray64 num2)
+        {
+            return !BitArray64.Equals(num1, num2);
+        }
+
+        //GetHashCode
+        public override int GetHashCode()
+        {
+            // returns a 32-bit  integer 
+            return this.number.GetHashCode();
+        }
+
+        //[]
+        public int this[int index]
+        {
+            get
+            {
+                if (GetIndex(index))
+                {
+                    throw new IndexOutOfRangeException("The index should be in the interval [0, 63]");
+                }
+                else
+                {
+                    int[] bitsArray = this.numberInBits;
+                    return bitsArray[index];
+                }
+            }
+
+            set
+            {
+                this.numberInBits[index] = value;
+            }
+        }
+
+        private bool GetIndex(int index)
+        {
+            if (index < 0 || index > 63)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
--- Week3/BitArray64/Program.cs
+++ Week3/BitArray64/Program.cs
@@ -6,7 +6,30 @@ namespace BitArray64
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            BitArray64 simpleBitArray = new BitArray64(10);
+
+            foreach (var bit in simpleBitArray)
+            {
+                Console.Write(bit);
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine(simpleBitArray[2] == simpleBitArray[62]);
+            Console.WriteLine("\n");
+
+
+            simpleBitArray[2] = 1;
+            simpleBitArray[10] = 1;
+
+            foreach (var bit in simpleBitArray)
+            {
+                Console.Write(bit);
+            }
+
+            Console.WriteLine("\n");
+            Console.WriteLine(simpleBitArray[2] == simpleBitArray[10]);
+
+            Console.Read();
         }
     }
 }
