f71bcc1b5980273e0f7cf3a92302e56a7bd352ca
Ionut Airinei <ionut.n.airinei@gmail.com>
2020-03-29 16:37:26 +0300

    Smooth sentences.

---
+++ week4/SmoothSentences/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SmoothSentences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write a smooth santance");
+            string smooth = Console.ReadLine();
+
+            if(IsSmooth(smooth))
+            {
+                Console.WriteLine("Is Smooth");
+            } else
+            {
+                Console.WriteLine("Is not Smooth");
+            }
+
+        }
+
+        private static bool IsSmooth(string smooth)
+        {
+            List<string> words = new List<string>();
+
+            words = smooth.Split(' ').ToList();
+
+            for (int i = 0; i < words.Count -1; i++)
+            {
+                var currentWord = words[i];
+                var nextWord = words[i + 1];
+
+                if(currentWord.Last() != nextWord.First())
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
