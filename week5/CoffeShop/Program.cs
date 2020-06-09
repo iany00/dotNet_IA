4bfc215a8c6d55fac37fffaca068228d8e36f591
Ionut Airinei <ionut.n.airinei@gmail.com>
2020-06-09 15:38:58 +0300

    Week 5

---
+++ week5/CoffeShop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CoffeShop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var coffee = new ChocolateDecorator(new Espresso());
+        }
+    }
+}
