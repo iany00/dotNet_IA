63ebbc026d5f0d864ca86fb03430c1c8b4dbcdff
Ionut Airinei <ionut.n.airinei@gmail.com>
2020-07-03 17:08:32 +0300

    week 10

---
+++ week10/Workshop/FirstAPI/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstAPI.Controllers
+{
+    //[Route("api/users")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public User Get(int id)
+        {
+            return new User()
+            {
+                UserId = id,
+                Email = "email",
+                UserName = "user name"
+            };
+        }
+    }
+}
